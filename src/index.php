ac6d4a4d464f3c987a18b32c4ae9a7335503c645
Foam Zou <woshizoufeng@qq.com>
2021-05-01 00:05:38 +0800

    fix bug: make sure the uuid match RFC

--- src/index.php
+++ src/index.php
@@ -3,7 +3,11 @@
 $query = empty($argv[1]) ? '' : $argv[1];
 
 function uuid_create(){
-    $str = md5(uniqid(mt_rand(), true));   
+    $data = random_bytes(16);
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+
+    $str = vsprintf('%s%s%s%s%s%s%s%s', str_split(bin2hex($data), 4));   
     $uuid  = substr($str,0,8) . '-';   
     $uuid .= substr($str,8,4) . '-';   
     $uuid .= substr($str,12,4) . '-';   
